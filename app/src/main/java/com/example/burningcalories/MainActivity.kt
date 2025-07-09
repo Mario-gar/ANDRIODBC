9a6bca78874dc3526d29705d0cda56f254a50016
mario <garciavallemario@gmail.com>
2025-07-08 22:43:14 -0600

    Todo

--- app/src/main/java/com/example/burningcalories/MainActivity.kt
+++ app/src/main/java/com/example/burningcalories/MainActivity.kt
@@ -1,47 +1,22 @@
 package com.example.burningcalories
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.burningcalories.AppNavigation.kt.AppNavigation
 import com.example.burningcalories.ui.theme.BURNINGCALORIESTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             BURNINGCALORIESTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navController = rememberNavController()
+                AppNavigation(navController)
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BURNINGCALORIESTheme {
-        Greeting("Android")
-    }
-}
